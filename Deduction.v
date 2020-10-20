cad0326d4f02a3bd189f7b93fb003e0499c5fde3
iehality <hodellurt@gmail.com>
2020-10-20 20:14:08 +0900

    add FOPL.v Deduction.v

---
+++ Deduction.v
@@ -0,0 +1,562 @@
+Require Import Classical.
+Require Import FunctionalExtensionality.
+Require Import PeanoNat.
+Require Import Coq.Arith.Le.
+Require Import Coq.Arith.Max.
+Require Import Lia.
+Require Import FOPL.FOPL.
+(* coqc -Q FOPL FOPL FOPL/Deduction.v *)
+
+Set Implicit Arguments.
+
+Section DeductionSystem.
+  
+  Variable L : Lang.
+  
+  Definition Th := LP -> Prop.
+  
+  (** * Hilbert Style Deduction System *)
+  
+  Definition sfT (T : Th) := fun p => exists q, p = sf q /\ T q.
+  
+  Inductive provable (T : Th) : LP ->  Prop :=
+  | GEN  : forall q, provable (sfT T) q -> provable T (fal q)
+  | MP   : forall p q, provable T p -> provable T (p --> q) -> provable T q
+  | Pr0  : forall p, T p -> provable T p
+  | Pr1  : forall p q, provable T (p --> q --> p)
+  | Pr2  : forall p q r, provable T ((p --> q --> r) --> ((p --> q) --> (p --> r)))
+  | Pr3  : forall p q, provable T ((~~ p --> ~~ q) --> (q --> p))
+  | Qt0  : forall p t, provable T (fal p --> p.(t))
+  | Qt1  : forall p q, provable T (fal (p --> q) --> ((fal p) --> (fal q)))
+  | Qt2  : forall p, provable T (p --> fal (sf p))
+  | Eq0  : forall t, provable T (t == t)
+  | Eq1  : forall p t u, provable T (t == u --> p.(t) --> p.(u)).
+  
+  Ltac MP h := apply MP with (p:=h).
+  Ltac GEN := apply GEN.
+  
+  Notation "T |- p" := (provable T p) (at level 95).
+  
+  Definition Consis (T : Th) := ~ exists p, (T |- p) /\ (T |- ~~ p).
+  
+  Definition addT (T : Th) p := fun x => T x \/ x = p.
+  Definition elmT (T : Th) p := fun x => T x /\ x <> p.
+  Definition includeT (T U : Th) := forall p, T p -> U p.
+  Notation "T :+ p" := (addT T p) (at level 71, left associativity).
+  Notation "T :- p" := (elmT T p) (at level 71, left associativity). 
+  Notation "T :< U" := (includeT T U) (at level 72, left associativity).
+  
+  Definition SentTh (T : Th) := T :< (sfT T) /\ (sfT T) :< T. 
+  
+  Lemma p__p : forall T p, T |- p --> p.
+  Proof.
+    intros.
+    MP (p --> (p --> p)).
+    apply Pr1.
+    MP ((p --> (p --> p) --> p)).
+    apply Pr1.
+    apply Pr2.
+  Qed.
+  
+  Ltac AX := (simpl; apply p__p) || apply Pr1 || apply Pr2 || apply Pr3 || apply Qt0 || apply Qt1 || apply Qt2 || apply Eq0 || apply Eq1 || unfold addT; apply Pr0; auto.
+  
+  (** ** Proof facts *)  
+  
+  Lemma imp_pr : forall T p q, (T |- p) -> (T |- q --> p).
+  Proof.
+    intros.
+    MP p.
+    auto.
+    AX.
+  Qed.
+  
+  Lemma imp_trans : forall T p q r, (T |- p --> q) -> (T |- q --> r) -> (T |- p --> r).
+  Proof.
+    intros.
+    MP (p --> q). auto.
+    MP (p --> q --> r).
+    apply imp_pr. auto.
+    AX.
+  Qed.
+  
+  Ltac TRANS h := apply imp_trans with (q:=h).
+  
+  Lemma TInclusion : forall (T U : Th) p, T :< U -> T |- p -> U |- p.
+  Proof.
+    assert (forall (T : Th) p, T |- p -> forall (U : Th), T :< U -> U |- p).
+    - intros T p H.
+      unfold includeT.
+      induction H.
+      + intros.
+        apply GEN.
+        apply IHprovable.
+        unfold sfT.
+        intros.
+        destruct H1 as [p0].
+        destruct H1.
+        exists p0.
+        auto.
+      + intros.
+        MP p.
+        auto. auto.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+      + intros. AX.
+    - intros.
+      apply H with (T := T).
+      auto.
+      auto.
+  Qed.
+  
+  Lemma weakening : forall T p q, (T |- q) -> (T :+ p |- q).
+  Proof.
+    intros.
+    apply TInclusion with (T := T).
+    unfold includeT. unfold addT. auto.
+    auto.
+  Qed.
+  
+  Theorem Deduction : forall T p q, (T :+ p |- q) -> (T |- p --> q).
+  Proof.
+    assert (forall T q, (T |- q) -> (forall p, T :- p |- p --> q)).
+    - intros T q H.
+      induction H.
+      + intros.
+        specialize (IHprovable (sf p)).
+        apply imp_trans with (q := fal (sf p)).
+        AX.
+        MP (fal (sf p --> q)).
+        GEN.
+        apply TInclusion with (T := sfT T :- sf p).
+        unfold sfT. unfold elmT. unfold includeT.
+        intros.
+        destruct H0.
+        destruct H0 as [p1].
+        destruct H0.
+        exists p1.
+        split. auto.
+        split. auto.
+        contradict H1.
+        rewrite <- H1.
+        auto.
+        auto.
+        AX.
+      + intros.
+        MP (p0 --> p). auto.
+        MP (p0 --> p --> q). auto.
+        AX.
+      + intros.
+        assert (em := classic (p0 = p)).
+        destruct em.
+        * rewrite <- H0.
+          AX.
+        * apply imp_pr.
+          AX.
+          split.
+          auto.
+          contradict H0.
+          symmetry.
+          auto.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+      + intros.
+        apply imp_pr.
+        AX.
+    - intros.
+      assert (em := classic (T p)).
+      destruct em.
+      + apply imp_pr. 
+        apply TInclusion with (T := T :+ p).
+        unfold includeT.
+        intros.
+        destruct H2.
+        auto.
+        rewrite -> H2.
+        auto.
+        auto.
+      + apply TInclusion with (T := (T :+ p) :- p).
+        unfold includeT.
+        intros.
+        destruct H2.
+        destruct H2.
+        auto.
+        contradiction.
+        auto.
+  Qed.
+  
+  Ltac INTRO := apply Deduction.
+  
+  Lemma sf_dsb0 : forall T p, (sfT T :+ sf p) :< (sfT (T :+ p)).
+  Proof.
+    intros.
+    unfold sfT. unfold addT. unfold includeT.
+    intros.
+    destruct H.
+    destruct H as [p1].
+    destruct H.
+    exists p1.
+    split. auto.
+    auto.
+    exists p.
+    split.
+    auto.
+    auto.
+  Qed.
+  
+  Lemma sf_dsb1 : forall T U p, T :< U -> (T :+ p) :< (U :+ p).
+  Proof.
+    unfold sfT. unfold addT. unfold includeT.
+    intros.
+    destruct H0.
+    auto.
+    auto.
+  Qed.
+  
+  Lemma sf_dsb : forall T p q, (sfT T :+ sf p |- q) -> (sfT (T :+ p) |- q).
+  Proof.
+    intros.
+    apply TInclusion with (T := sfT T :+ sf p).
+    apply sf_dsb0.
+    auto.
+  Qed.
+  
+  Lemma imp_pqrs : forall T p q r s, (T |- p --> q --> r) -> (T |- r --> s) -> (T |- p --> q --> s).
+  Proof.
+    intros.
+    MP (p --> q --> r). auto.
+    MP (p --> (q --> r) --> (q --> s)).
+    apply imp_pr.
+    MP (q --> r --> s).
+    apply imp_pr. auto.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma explosion : forall T, (~ Consis T) -> forall p, T |- p.
+  Proof.
+    intros.
+    unfold Consis in H. apply NNPP in H.
+    destruct H as [q].
+    destruct H.
+    MP q. auto.
+    MP (~~ p --> ~~ q).
+    MP (~~ q).
+    auto.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma p_np_q : forall T p q, T |- p --> ~~ p --> q.
+  Proof.
+    intros.
+    INTRO.
+    INTRO.
+    apply explosion.
+    unfold Consis.
+    intro.
+    contradict H.
+    exists p.
+    split.
+    AX.
+    AX.
+  Qed.
+    
+  Lemma deduction_inv : forall T p q, (T |- p --> q) -> (T :+ p |- q).
+  Proof.
+    intros.
+    MP p. AX.
+    apply weakening.
+    auto.
+  Qed.
+  
+  Lemma pr_NNPP : forall T p, T |- ~~ ~~ p --> p.
+  Proof.
+    intros.
+    INTRO.
+    MP (~~ ~~ p).
+    AX.
+    MP (~~ p --> ~~ ~~ ~~ p).
+    MP (~~ ~~ ~~ ~~ p --> ~~ ~~ p).
+    MP (~~ ~~ p).
+    AX.
+    AX.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma pr_NN : forall T p, T |- p --> ~~ ~~ p.
+  Proof.
+    intros.
+    MP (~~ ~~ ~~ p --> ~~ p).
+    apply pr_NNPP.
+    AX.
+  Qed.
+  
+  Lemma pNNPP : forall T p, (T |- ~~ ~~ p) -> (T |- p).
+  Proof.
+    intros.
+    MP (~~ ~~ p).
+    auto.
+    MP (~~ p --> ~~ ~~ ~~ p).
+    MP (~~ ~~ ~~ ~~ p --> ~~ ~~ p).
+    MP (~~ ~~ p).
+    auto.
+    AX.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma pNN : forall T p, (T |- p) -> (T |- ~~ ~~ p).
+  Proof.
+    intros.
+    MP p. auto.
+    MP (~~ ~~ ~~ p --> ~~ p).
+    apply pr_NNPP.
+    AX.
+  Qed.
+  
+  Lemma neg_intro : forall T p, T |- (p --> ~~ p) -> (T |- ~~ p).
+  Proof.
+    intros.
+    MP (p --> ~~ p). auto.
+    INTRO.
+    MP (p --> ~~ p).
+    AX.
+    MP (~~ ~~ p --> ~~ (p --> ~~ p)).
+    INTRO.
+    apply explosion.
+    unfold Consis.
+    intro. contradict H0.
+    exists p.
+    split.
+    apply pNNPP. AX.
+    MP p.
+    apply pNNPP. AX.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma contrad_elim : forall T p q, (T |- ~~ p --> ~~ q) -> (T |- q --> p).
+  Proof.
+    intros.
+    MP (~~ p --> ~~ q). auto.
+    AX.
+  Qed.
+  
+  Lemma contrad_add : forall T p q, (T |- p --> q) -> (T |- ~~ q --> ~~ p).
+  Proof.
+    intros.
+    MP (~~ ~~ p --> ~~ ~~ q).
+    INTRO.
+    MP q.
+    MP p.
+    MP (~~ ~~ p). AX.
+    apply pr_NNPP.
+    apply weakening.
+    auto.
+    apply pr_NN.
+    AX.
+  Qed.
+  
+  Lemma destruct_and : forall T p q, (T |- p) -> (T |- q) -> (T |- p //\ q).
+  Proof.
+    unfold andl.
+    intros.
+    apply neg_intro.
+    - INTRO.
+      MP q.
+      apply weakening.
+      auto.
+      auto.
+      MP (~~ ~~ (p --> ~~ q) --> ~~ q).
+      apply imp_pr.
+      MP p.
+      apply weakening.
+      auto. auto.
+      AX.
+      AX.
+  Qed.
+  
+  Lemma and_destruct : forall T p q, (T |- p //\ q) -> ((T |- p) /\ (T |- q)).
+  Proof.
+    unfold andl.
+    intros.
+    split.
+    - MP (~~ (p --> ~~ q)). auto.
+      MP (~~ p --> ~~ ~~ (p --> ~~ q)).
+      INTRO.
+      MP (p --> ~~ q).
+      INTRO.
+      apply explosion.
+      unfold Consis. intro. contradict H0. 
+      exists p. split. AX. AX.
+      apply pr_NN.
+      AX.
+    - MP (~~ (p --> ~~ q)). auto.
+      MP (~~ q --> ~~ ~~ (p --> ~~ q)).
+      INTRO.
+      MP (p --> ~~ q).
+      apply imp_pr. AX.
+      apply pr_NN.
+      AX.
+  Qed.
+  
+  Lemma destruct_iff : forall T p q, (T |- p --> q) -> (T |- q --> p) -> (T |- p <--> q).
+  Proof.
+    intros.
+    apply destruct_and.
+    auto.
+    auto.
+  Qed.
+  
+  Ltac SPLIT := apply destruct_iff || apply destruct_and.
+  Ltac DESTRUCT h := apply and_destruct in h; destruct h.
+  
+  Lemma pr_rewrite2 : forall {T} {p0 p1 r}, (T |- p0 <--> p1) -> (T |- p0 --> r) -> (T |- p1 --> r).
+  Proof.
+    intros.
+    DESTRUCT H.
+    apply imp_trans with (q := p0). 
+    auto.
+    auto.
+  Qed.
+  
+  Lemma subst : forall T p t, (T |- fal p) -> (T |- p.(t)).
+  Proof.
+    intros.
+    MP (fal p). auto.
+    AX.
+  Qed.
+  
+  Ltac SPECIALIZE h u := apply subst with (t := u) in h.
+  
+  Lemma fal_and_destruct : forall T p q, (T |- fal (p //\ q)) -> ((T |- fal p) /\ (T |- fal q)).
+  Proof.
+    intros.
+    split.
+    - MP (fal (p //\ q)). auto.
+      MP (fal ((p //\ q) --> p)).
+      GEN.
+      INTRO.
+      assert (sfT T :+ (p //\ q) |- p //\ q). AX.
+      DESTRUCT H0.
+      auto.
+      AX.
+    - MP (fal (p //\ q)). auto.
+      MP (fal ((p //\ q) --> q)).
+      GEN.
+      INTRO.
+      assert (sfT T :+ (p //\ q) |- p //\ q). AX.
+      DESTRUCT H0.
+      auto.
+      AX.
+  Qed.
+  
+  Lemma fal_trans : forall T p q r, (T |- fal (p --> q)) -> (T |- fal (q --> r)) -> (T |- fal (p --> r)).
+  Proof.
+    intros.
+    MP (fal (p --> q)). auto.
+    MP (fal ((p --> q) --> p --> r)).
+    MP (fal (p --> q --> r)).
+    MP (fal (q --> r)). auto.
+    MP (fal ((q --> r) --> (p --> q --> r))).
+    GEN.
+    repeat INTRO.
+    MP q.
+    AX.
+    AX.
+    AX.
+    MP (fal ((p --> q --> r) --> (p --> q) --> (p --> r))).
+    GEN.
+    AX.
+    AX.
+    AX.
+  Qed.
+  
+  Lemma eql_refl : forall T t u, (T |- t == u) -> (T |- u == t).
+  Proof.
+    intros.
+    MP (t == u). auto.
+    INTRO.
+    MP (t == t). AX.
+    MP (t == u). AX.
+    assert (forall v, (v == t) = ('0 == sfc t).(v)).
+    unfold sfc.
+    intros.
+    simpl.
+    rewrite <- nested_rewc.
+    simpl.
+    rewrite <- rewc_id.
+    auto.
+    rewrite -> H0.
+    rewrite -> H0.
+    AX.
+  Qed.
+
+  Lemma eql_trans : forall T t u v, (T |- t == u) -> (T |- u == v) -> (T |- t == v).
+  Proof.
+    intros.
+    MP (u == v). auto.
+    MP (u == t).
+    apply eql_refl. auto.
+    assert (forall x, (x == v) = ('0 == sfc v).(x)).
+    unfold sfc.
+    intros.
+    simpl.
+    rewrite <- nested_rewc.
+    simpl.
+    rewrite <- rewc_id.
+    auto.
+    rewrite -> H1.
+    rewrite -> H1.
+    AX.
+  Qed.
+  
+End DeductionSystem.
+
+Arguments Th {_}.
+Arguments provable {_}.
+Arguments addT {_}.
+Arguments elmT {_}.
+Arguments includeT {_}.
+
+Notation "T |- p" := (provable T p) (at level 95).
+Notation "T :+ p" := (addT T p) (at level 71, left associativity).
+Notation "T :- p" := (elmT T p) (at level 71, left associativity). 
+Notation "T :< U" := (includeT T U) (at level 72, left associativity).
+
+Ltac GEN := apply GEN.
+Ltac MP h := apply MP with (p:=h).
+Ltac AX := apply p__p || apply Pr1 || apply Pr2 || apply Pr3 || apply Qt0 || apply Qt1 || apply Qt2 || apply Eq0 || apply Eq1 || unfold addT; apply Pr0; auto.
+Ltac TRANS h := apply imp_trans with (q:=h).
+Ltac INTRO := apply Deduction.
+Ltac SPLIT := apply destruct_iff || apply destruct_and.
+Ltac DESTRUCT h := apply and_destruct in h; destruct h.
+Ltac SPECIALIZE h u := apply subst with (t := u) in h.
+Ltac SYMMETRY := apply eql_refl.
+Ltac WL := apply weakening.
---
+++ FOPL.v
@@ -0,0 +1,516 @@
+Require Import Classical.
+Require Import FunctionalExtensionality.
+Require Import PeanoNat.
+Require Import Coq.Arith.Le.
+Require Import Coq.Arith.Max.
+Require Import Lia.
+(* coqc -Q FOPL FOPL FOPL/FOPL.v *)
+
+
+Section FirstOrderPredicateLogic.
+(**)
+
+  Lemma lt_lt_max_l : forall n m r, n < m -> n < max m r.
+  Proof.
+    unfold lt.
+    intros.
+    assert (lml := (Nat.le_max_l m r)).
+    apply Nat.le_trans with (m := m).
+    auto.
+    auto.
+  Qed.
+
+  Lemma lt_lt_max_r : forall n m r, n < m -> n < max r m.
+  Proof.
+    unfold lt.
+    intros.
+    assert (lml := (Nat.le_max_r r m)).
+    apply Nat.le_trans with (m := m).
+    auto.
+    auto.
+  Qed.
+
+  Lemma le_le_max_l : forall n m r, n <= m -> n <= max m r.
+  Proof.
+    intros.
+    assert (lml := (Nat.le_max_l m r)).
+    apply Nat.le_trans with (m := m).
+    auto.
+    auto.
+  Qed.
+
+  Lemma le_le_max_r : forall n m r, n <= m -> n <= max r m.
+  Proof.
+    intros.
+    assert (lml := (Nat.le_max_r r m)).
+    apply Nat.le_trans with (m := m).
+    auto.
+    auto.
+  Qed.
+
+(**)
+
+  Class Lang := {fc0 : Type; fc1 : Type; fc2 : Type; pd0 : Type; pd1 : Type; pd2 : Type}.
+  Variable L : Lang.
+
+  Inductive LC :=
+  | var : nat -> LC
+  | cns : LC
+  | Fc0 : fc0 -> LC
+  | Fc1 : fc1 -> LC -> LC
+  | Fc2 : fc2 -> LC -> LC -> LC.
+
+  Inductive LP :=
+  | eql : LC -> LC -> LP
+  | Pd0 : pd0 -> LP
+  | Pd1 : pd1 -> LC -> LP
+  | Pd2 : pd2 -> LC -> LC -> LP
+  | imp : LP -> LP -> LP
+  | neg : LP -> LP
+  | fal : LP -> LP.
+
+  Definition andl p q := neg (imp p (neg q)).
+  Definition orl p q := imp (neg p) q.
+
+  Definition slide {A : Type} (s : nat -> A) (n : A) : nat -> A := fun x0 => 
+    match x0 with
+    | 0 => n
+    | S m => s m
+    end.
+  
+  Notation "( n ; s )" := (slide s n) (at level 0).
+
+  Fixpoint rewc (s : nat -> LC) (c : LC) : LC :=
+    match c with
+    | var v     => s v
+    | cns      => cns
+    | Fc0 c     => Fc0 c
+    | Fc1 c x   => Fc1 c (rewc s x)
+    | Fc2 c x y => Fc2 c (rewc s x) (rewc s y)
+    end.
+
+  Definition sfc (c : LC) := rewc (fun x => (var (S x))) c.
+
+  Fixpoint rew (s : nat -> LC) (p0 : LP) : LP :=
+    match p0 with
+    | eql x y   => eql (rewc s x) (rewc s y)
+    | Pd0 c     => Pd0 c
+    | Pd1 c x   => Pd1 c (rewc s x)
+    | Pd2 c x y => Pd2 c (rewc s x) (rewc s y)
+    | imp p q   => imp (rew s p) (rew s q)
+    | neg p     => neg (rew s p)
+    | fal p     => fal (rew ((var 0); fun x => (sfc (s x))) p)
+    end.
+
+  Notation "p [ s ]" := (rew s p) (at level 0).
+  Notation "p [ n ; s ]" := (p [(n;s)]) (at level 0).
+  Notation "\0" := (fun x => (var x)) (at level 0).
+
+  Definition sf (p : LP) : LP := p [fun x => (var (S x))].
+  Definition norm c p := p [fun x => c].
+  Notation "p .( x )" := (p [x;\0]) (at level 0).
+  Notation "p .( x , y )" := (p [x; (y; \0)]) (at level 0).
+  Notation "p .( x , y , z )" := (p [x; (y; (z; \0))]) (at level 0).
+
+(** ** Syntax Facts *)
+
+  Lemma nested_rewc : forall s0 s1 t, rewc (fun x => rewc s1 (s0 x)) t = (rewc s1 (rewc s0 t)).
+  Proof.
+    intros.
+    induction t.
+    - simpl.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      rewrite -> IHt.
+      auto.
+    - simpl.
+      rewrite -> IHt1.
+      rewrite -> IHt2.
+      auto.
+  Qed.
+
+  Lemma rewcsf_rwec : forall t0 s t, rewc (t0;s) (sfc t) = rewc s t.
+  Proof.
+    intros. unfold sfc.
+    induction t.
+    - simpl.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      rewrite -> IHt.
+      auto.
+    - simpl.
+      rewrite -> IHt1.
+      rewrite -> IHt2.
+      auto.
+  Qed.
+
+  Lemma nested_rew : forall p s0 s1, p [fun x => rewc s1 (s0 x)] = p [s0] [s1].
+  Proof.
+    induction p.
+    - simpl.
+      intros.
+      rewrite <- nested_rewc.
+      rewrite <- nested_rewc.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- nested_rewc.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- nested_rewc.
+      rewrite <- nested_rewc.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- IHp1.
+      rewrite <- IHp2.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- IHp.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- IHp.
+      assert ( ((var 0); fun x => sfc (rewc s1 (s0 x))) = (fun x => rewc ((var 0); fun x0 => sfc (s1 x0)) (((var 0); fun x0 => sfc (s0 x0)) x))).
+      + apply functional_extensionality.
+        intros.
+        destruct x.
+        simpl.
+        auto.
+        simpl.
+        rewrite -> rewcsf_rwec.
+        unfold sfc.
+        rewrite <- nested_rewc.
+        auto.
+      + rewrite <- H.
+        auto.
+  Qed.
+
+  Lemma neg_sbs : forall p t, (neg p).(t) = neg p.(t).
+  Proof.
+    intros.
+    simpl.
+    auto.
+  Qed.
+
+  (** Ar $n$ $p$ $\iff$ $p$ has a variables $(varm)$ for $n \leq m$ *)
+  
+  Fixpoint Art (t0 : LC) : nat :=
+    match t0 with
+    | var n     => S n
+    | cns      => 0
+    | Fc0 _     => 0
+    | Fc1 _ x   => Art x
+    | Fc2 _ x y => max (Art x) (Art y)
+    end.
+
+  Fixpoint Ar (p0 : LP) : nat :=
+    match p0 with
+    | eql t u   => max (Art t) (Art u)
+    | Pd0 _     => 0
+    | Pd1 _ x   => Art x
+    | Pd2 _ x y => max (Art x) (Art y)
+    | imp p q   => max (Ar p) (Ar q)
+    | neg p     => Ar p
+    | fal p     => Ar p - 1
+    end.
+
+  Lemma rewc_rewc : forall t s0 s1, (forall n, n < Art t -> s0 n = s1 n) -> rewc s0 t = rewc s1 t.
+  Proof.
+    intros.
+    induction t.
+    - simpl.
+      apply H.
+      simpl.
+      lia.
+    - simpl.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      assert (rewc s0 t = rewc s1 t).
+      apply IHt.
+      intros.
+      apply H.
+      simpl.
+      auto.
+      rewrite <- H0.
+      auto.
+    - simpl.
+      assert (rewc s0 t1 = rewc s1 t1).
+      apply IHt1.
+      intros.
+      apply H.
+      simpl.
+      apply lt_lt_max_l. auto.
+      assert (rewc s0 t2 = rewc s1 t2).
+      apply IHt2.
+      intros.
+      apply H.
+      simpl.
+      apply lt_lt_max_r. auto.
+      rewrite <- H0. 
+      rewrite <- H1.
+      auto.
+  Qed.
+
+  Lemma rew_rew : forall p s0 s1, (forall n, n < Ar p -> s0 n = s1 n) -> p [s0] = p [s1].
+  Proof.
+    induction p.
+    - simpl.
+      intros.
+      assert (rewc s0 l = rewc s1 l).
+      apply rewc_rewc.
+      intros.
+      apply H.
+      apply lt_lt_max_l. auto.
+      assert (rewc s0 l0 = rewc s1 l0).
+      apply rewc_rewc.
+      intros.
+      apply H.
+      apply lt_lt_max_r. auto.
+      rewrite <- H0.
+      rewrite <- H1.
+      auto.
+    - simpl.
+      auto.
+    - simpl.
+      intros.
+      assert (rewc s0 l = rewc s1 l).
+      apply rewc_rewc.
+      intros.
+      auto.
+      rewrite <- H0.
+      auto.
+    - simpl.
+      intros.
+      assert (rewc s0 l = rewc s1 l).
+      apply rewc_rewc.
+      intros.
+      apply H.
+      apply lt_lt_max_l. auto.
+      assert (rewc s0 l0 = rewc s1 l0).
+      apply rewc_rewc.
+      intros.
+      apply H.
+      apply lt_lt_max_r. auto.
+      rewrite <- H0.
+      rewrite <- H1.
+      auto.
+    - simpl.
+      intros.
+      assert (p1 [s0] = p1 [s1]).
+      + apply IHp1.
+        intros.
+        apply H.
+        apply lt_lt_max_l.
+        auto.
+      + assert (p2 [s0] = p2 [s1]).
+        apply IHp2.
+        intros.
+        apply H.
+        apply lt_lt_max_r.
+        auto.
+      rewrite <- H0.
+      rewrite <- H1.
+      auto.
+    - simpl.
+      intros.
+      rewrite <- (IHp s0 s1 H).
+      auto.
+    - simpl.
+      intros.
+      assert (p [(var 0); fun x => sfc (s0 x)] = p [(var 0); fun x => sfc (s1 x)]).
+      + apply IHp.
+        unfold sfc. intros.
+        destruct n.
+        simpl. auto.
+        simpl.
+        assert (s0 n = s1 n).
+        * apply H.
+          lia.
+        * rewrite <- H1.
+          auto.
+      + rewrite <- H0.
+        auto.
+  Qed.
+
+  Lemma rewc_id : forall t, t = rewc \0 t.
+  Proof.
+    induction t.
+    - simpl. auto.
+    - simpl. auto.
+    - simpl. auto.
+    - simpl. 
+      rewrite <- IHt.
+      auto.
+    - simpl.
+      rewrite <- IHt1.
+      rewrite <- IHt2.
+      auto.
+  Qed.
+
+  Lemma rew_id : forall p, p = p [\0].
+  Proof.
+    induction p.
+    - simpl. 
+      rewrite <- rewc_id.
+      rewrite <- rewc_id.
+      auto.
+    - simpl. auto.
+    - simpl. 
+      rewrite <- rewc_id.
+      auto.
+    - simpl.
+      rewrite <- rewc_id.
+      rewrite <- rewc_id.
+      auto.
+    - simpl.
+      rewrite <- IHp1.
+      rewrite <- IHp2.
+      auto.
+    - simpl.
+      rewrite <- IHp.
+      auto.
+    - simpl.
+      assert (\0 = ((var 0); fun x => sfc (var x))).
+      + unfold sfc.
+        apply functional_extensionality.
+        intros.
+        destruct x.
+        simpl.
+        auto.
+        simpl.
+        auto.
+      + rewrite <- H.
+        rewrite <- IHp.
+        auto.
+  Qed.
+
+  Lemma constant_rew : forall t s, Art t = 0 -> t = rewc s t.
+  Proof.
+    intros.
+    assert ( rewc \0 t = rewc s t).
+    - apply rewc_rewc.
+      rewrite -> H.
+      lia.
+    - rewrite <- H0.
+      apply rewc_id.
+  Qed. 
+
+  Lemma sentence_rew : forall p s, Ar p = 0 -> p = p [s].
+  Proof.
+    intros.
+    assert (p [\0] = p [s]).
+    - apply rew_rew.
+      rewrite -> H.
+      intros.
+      destruct n.
+      lia.
+      lia.
+    - rewrite <- H0.
+      apply rew_id.
+  Qed. 
+
+  Lemma form1_p_ps : forall p s, Ar p = 1 -> (var 0 = s 0) -> p = p [s].
+  Proof.
+    intros.
+    assert (p [\0] = p [s]).
+    - apply rew_rew.
+      rewrite -> H.
+      intros.
+      destruct n.
+      auto.
+      lia.
+    - rewrite <- H1.
+      apply rew_id.
+  Qed. 
+
+  Lemma form2_ps_ps : forall p s0 s1, Ar p = 2 -> (s0 0 = s1 0) -> (s0 1 = s1 1) -> p [s0] = p [s1].
+  Proof.
+    intros.
+    apply rew_rew.
+    rewrite -> H.
+    intros.
+    destruct n.
+    auto.
+    destruct n.
+    auto.
+    lia.
+  Qed. 
+
+  Lemma form3_ps_ps : forall p s0 s1, Ar p = 3 -> (s0 0 = s1 0) -> (s0 1 = s1 1) -> (s0 2 = s1 2) -> p [s0] = p [s1].
+  Proof.
+    intros.
+    apply rew_rew.
+    intros.
+    destruct n. auto.
+    destruct n. auto.
+    destruct n. auto.
+    lia.
+  Qed.
+
+  Lemma form3_rw0 : forall p s, Ar p = 3 -> p.(s 0, s 1, s 2) = p [s].
+  Proof.
+    intros.
+    apply rew_rew.
+    rewrite -> H.
+    intros.
+    destruct n.
+    simpl. auto.
+    destruct n.
+    simpl. auto.
+    destruct n.
+    simpl. auto.
+    lia.
+  Qed.
+
+End FirstOrderPredicateLogic.
+
+Arguments cns {_}.
+Arguments var {_}.
+Arguments eql {_}.
+Arguments neg {_}.
+Arguments imp {_}.
+Arguments andl {_}.
+Arguments orl {_}.
+Arguments LC {_}.
+Arguments LP {_}.
+Arguments fal {_}.
+Arguments rewc {_}.
+Arguments rew {_}.
+Arguments Art {_}.
+Arguments Ar {_}.
+Arguments sfc {_}.
+Arguments sf {_}.
+
+Notation "( n ; s )" := (slide s n) (at level 0).
+Notation "p [ s ]" := (rew s p) (at level 0).
+Notation "p [ n ; s ]" := (p [(n;s)]) (at level 0).
+Notation "\0" := (fun x => (var x)) (at level 0).
+
+Notation "p .( x )" := (p [x;\0]) (at level 0).
+Notation "p .( x , y )" := (p [x; (y; \0)]) (at level 0).
+Notation "p .( x , y , z )" := (p [x; (y; (z; \0))]) (at level 0).
+
+Notation "`0" := (cns) (at level 0).
+Notation "' v " := (var v) (at level 0).
+Notation "a == b" := (eql a b) (at level 70, right associativity).
+Notation "~~ p" := (neg p) (at level 79, right associativity).
+Notation "p --> q" := (imp p q) (at level 80, right associativity, q at level 200).
+
+Notation "p //\ q" := (andl p q) (at level 81, right associativity).
+Notation "p \\/ q" := (orl p q) (at level 82, right associativity).
+Notation "p <--> q" := ((p --> q) //\ (q --> p)) (at level 83, right associativity, q at level 200).
